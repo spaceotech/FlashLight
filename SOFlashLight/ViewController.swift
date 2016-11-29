e7a8ea50497cafe589f945c669aa8e7a39e9f2c6
hiteshtrivedi <hitesh_trivedi@spaceotechnologies.com>
2016-11-29 16:59:27 +0530

    First Commit

---
+++ SOFlashLight/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  SOFlashLight
+//
+//  Created by Hitesh on 11/29/16.
+//  Copyright © 2016 myCompany. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var isTourch = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func actionTorchClick(sender: AnyObject) {
+        
+        guard let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo) else { return }
+        
+        if device.hasTorch {
+            isTourch = !isTourch
+            do {
+                try device.lockForConfiguration()
+                
+                if isTourch == true {
+                    device.torchMode = .On
+                } else {
+                    device.torchMode = .Off
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
